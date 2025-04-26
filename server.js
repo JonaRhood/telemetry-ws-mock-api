f05e4b196ff7076d7dfc69ab987ca26fb4566960
Jonathan Cano <jonathancanofreta@gmail.com>
2025-04-26 20:17:22 +0200

    First Commit

---
+++ server.js
@@ -0,0 +1,46 @@
+// server.js
+
+require('dotenv').config();
+const express = require('express');
+const http = require('http');
+const WebSocket = require('ws');
+
+const port = parseInt(process.env.PORT, 10) || 8080;
+
+const app = express();
+const server = http.createServer(app);
+const wss = new WebSocket.Server({ server, path: '/ws' });
+
+
+wss.on('connection', ws => {
+  console.log('Cliente conectado');
+});
+
+
+const interval = setInterval(() => {
+
+  wss.clients.forEach(client => {
+    if (client.readyState === WebSocket.OPEN) {
+      let data = [];
+
+      for (let i = 1; i <= 20; i++) {
+        data.push({
+          id: i,
+          heartRate: Math.floor(Math.random() * 40) + 60,
+          oxygen: Math.floor(Math.random() * 10) + 90,
+          timestamp: new Date().toISOString(),
+        });
+
+        console.log(data);
+      }
+      client.send(JSON.stringify(data));
+    }
+  });
+
+}, 2000);
+
+app.get('/health', (req, res) => res.status(200).send('OK'));
+
+server.listen(port, () => {
+  console.log(`🚀 Server ready on http://localhost:${port}`);
+});
